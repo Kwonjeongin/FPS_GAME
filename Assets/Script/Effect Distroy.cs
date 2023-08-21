d2af0856db035d673f5b5a52521e153fef087a37
Kwonjeongin <ad160515@gmail.com>
2023-08-21 13:36:57 +0900

    총알발사 crossfire ,  적 팔로잉
    총알발사 crossfire
    -마우스 왼쪽 버튼을 누르면 시선 방향으로 총을 발사
    
    적 팔로잉
    -적의 상태가 Move 일때 플레이어와의 거리가 공격범위 밖이면,적이 플레이어를 따라간다.

--- Assets/Script/Effect Distroy.cs
+++ Assets/Script/Effect Distroy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 // 목적 : (내)이펙트가 특정 시간이 지나면 제거 된다.
 // 필요속성 : 현재 시간 , 특정시간
+
+//목적2  : 
 public class EffectDistroy : MonoBehaviour
 {
     // 필요속성 : 현재 시간 , 특정시간
---
+++ Assets/Script/EnemyFsm.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+// 목표: 적을 FSM 다이어그램에 따라 동작시키고 싶다.
+// 필요속성1: 적 상태
+
+// 목표2: 플레이어와의 거리를 측정해서 특정 상태로 만들어준다.
+// 필요속성2: 플레이어와의 거리, 플레이어 트랜스폼
+
+// 목표3 : 적의 상태가 Move 일때 플레이어와의 거리가 공격범위 밖이면,적이 플레이어를 따라간다.
+// 필요속성 : 이동속도, 적을 위한 캐릭터 컨트롤러,공격범위
+public class EnemyFSM : MonoBehaviour
+{
+    // 필요속성: 적 상태
+    enum EnemyState
+    {
+        Idle,
+        Move,
+        Attack,
+        Return,
+        Damaged,
+        Die
+    }
+    EnemyState enemyState;
+
+    // 필요속성: 플레이어와의 거리, 플레이어 트랜스폼
+    public float findDistance = 5f;
+    Transform player;
+
+    // 필요속성 : 이동속도, 적을 위한 캐릭터 컨트롤러,공격범위
+    public float moveSpeed;
+    CharacterController characterController;
+    public float attackDistance = 2f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyState = EnemyState.Idle;
+
+        player = GameObject.Find("Player").transform;
+
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 목표: 적을 FSM 다이어그램에 따라 동작시키고 싶다.
+        switch (enemyState)
+        {
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Move:
+                Move();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Return:
+                Return();
+                break;
+            case EnemyState.Damaged:
+                Damaged();
+                break;
+            case EnemyState.Die:
+                Die();
+                break;
+        }
+    }
+
+    private void Die()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Damaged()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Return()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Move()
+    {
+        // 목표3 : 적의 상태가 Move 일때 플레이어와의 거리가 공격범위 밖이면,적이 플레이어를 따라간다.
+        float distanceToPlayer = (player.position - transform.position).magnitude;
+        if (distanceToPlayer > attackDistance)
+        {
+            Vector3 dir = (player.position - transform.position).normalized;
+
+            //플레이어를 따라간다.
+            characterController.Move(dir * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //공격 범위내로 들어오면 공격으로 상태를 전환한다.
+            enemyState = EnemyState.Idle;
+            print("상태전환 -> Attack");
+        }
+    }
+
+    private void Idle()
+    {
+        float distanceToPlayer = (player.position - transform.position).magnitude;
+        // float tempDist = Vector3.Distance(transform.position, player.position);
+
+        // 현재 플레이어와의 거리가 특정 범위 내면 상태를 Move로 바꿔준다.
+        if (distanceToPlayer < findDistance)
+        {
+            enemyState = EnemyState.Move;
+            print("상태전환: Idle -> Move");
+        }
+    }
+}
